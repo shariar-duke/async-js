7beb5ab5d7ecfb03bc28f709e6c4e8f5a44b228f
shariar <shariarmahmud225@gmail.com>
2024-07-12 02:41:37 +0600

    nullish operator

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-// Example 4: Destructuring Arrays Within Objects
-const product = {
-    id: 202,
-    name: "Smartphone",
-    specs: {
-        dimensions: [150, 75, 7.7],
-        weight: 180,
-        features: ["4G", "5G", "WiFi", "Bluetooth"]
-    }
-};
+// Nullish Operator
 
-// Destructuring arrays within objects
-const {
-    specs: {
-        dimensions: [length, width, height],
-        features: [feature1, feature2, ...otherFeatures]
-    }
-} = product;
+let lang = null;
 
-console.log(length);       // Output: 150
-console.log(width);        // Output: 75
-console.log(height);       // Output: 7.7
-console.log(feature1);     // Output: "4G"
-console.log(feature2);     // Output: "5G"
-console.log(otherFeatures);// Output: ["WiFi", "Bluetooth"]
+console.log(lang ?? "Java")
+
+// There are two things . That we can call it is nullish syntax . One is null and undefined
