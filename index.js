0db0f3c2f876ab2a2e889f1aeecebbcc11ae2840
shariar <shariarmahmud225@gmail.com>
2024-07-11 20:04:16 +0600

    array splice

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Filter
 let number = [10, 12, 21, 24, 36, 45]
-const result = number.slice(0, 3)
-console.log(result)
-console.log(number)
-// Main array te hat dy nai
-// [ 10, 12, 21 ]
+const result = number.splice(0, 3)
+console.log(result) // reslut er modhe dibe remove kore ber kore ana element  [ 10, 12, 21 ]
+console.log(number) // r main array te dbe ber kore anar por ki thake se gula [ 24, 36, 45 ]
+
+
+// Array splice main array te hat dei . j tuk splice kore nibo ota k nia ekta new array banabe jeta result array ta.
+// r kate niye jawar por jeta baki thaklo seta k nia pore thakbe main array ta 
