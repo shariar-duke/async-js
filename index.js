08f40f17227e409a3fc100d12db08969f17abbec
shariar <shariarmahmud225@gmail.com>
2024-07-11 20:05:12 +0600

    array reudce

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// Filter
-let number = [10, 12, 21, 24, 36, 45]
-const result = number.splice(0, 3)
-console.log(result) // reslut er modhe dibe remove kore ber kore ana element  [ 10, 12, 21 ]
-console.log(number) // r main array te dbe ber kore anar por ki thake se gula [ 24, 36, 45 ]
+// Array Reduce
+const numbers = [1, 2, 3, 4, 6]
 
+// const result = numbers.reduce((total, current) => total+current,0)
+const result = numbers.reduce((total, current) => {
+    return total + current  // jode call back function er modhe { } thake tahle return kore dite hbe 
+}, 0)
 
-// Array splice main array te hat dei . j tuk splice kore nibo ota k nia ekta new array banabe jeta result array ta.
-// r kate niye jawar por jeta baki thaklo seta k nia pore thakbe main array ta 
+console.log(result)
