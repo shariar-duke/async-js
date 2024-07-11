8ba85dd0c8813bc8a01fbb76cb17f69f23ab4d07
shariar <shariarmahmud225@gmail.com>
2024-07-11 19:39:53 +0600

    returning object from a function

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-// most famous arrow function 
+// If I want to return a object from an function what should I do ?
 
-let hello = () => {
-    console.log("Hello world")
+
+const hello = () =>
+(
+    {
+        a: 10,
+        b: 20
+    }
+)
+
+console.log(hello())
+
+// jode amk response hisebe ekta object send kore dite hoy tahole amk => er por {a:5, b:5} dile hbe na tahle eta k object er bracket vabbe na vabbe holo new block of code tai object
+// patahte gele age { } er age () dite hbe jate ei {} take object er bracket vabe
+
+
+// Antoher way 
+
+const hi = () => {
+    return {
+        a: 10,
+        b: 20
+    }
 }
 
-hello()
+console.log(hi())
+
+// jdoe arrow er por {} use krei thake tahole pore r ekta return use kore nite hbe 
