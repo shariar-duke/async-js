7cda8c8bd8deabc6460fa3fc30abffb7f01853e0
shariar <shariarmahmud225@gmail.com>
2024-07-11 19:55:48 +0600

    general array.map

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// Array.filter 
+// Array.map
 const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"]
 
-const result = fruits.filter((f) => f === "Mango" || f === "Man")
+const result = fruits.map((f) => f === "Mango" || f === "Man")
 console.log(result)
 
-// array.filer sob gular modhe dia jabe gia ddekhbe kara kara condition meet kore jara jara meet kroe tader nia new array creat korbe
-// array.find return korto holo ekta single value r filter return korbe holo array . sei array te tarai thakbe jader sathe condition mile
-// result  [ 'Mango' ] . ei array ta te aro onk element thakte pare . 
+// result  [ false, false, false, false, true ]
+// karon array.map korle result er modhe same length er array create hbe . ekhne condition millo na tai true false
+// array map a generally emn kisu kroe na . Array map a jeta kroe set holo protibar j elmeent er access pasch tar upor kno kisu kora labe naki seta kore return krer 
