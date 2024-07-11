5f07f5f11509829a9bb7a346d861660359295476
shariar <shariarmahmud225@gmail.com>
2024-07-11 19:20:42 +0600

    conscoept of fucntion scope with example

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-var lang = "javaScript"
-// global schope
 
-function learn(topic) {
-
-    // this lang is inside the function scope 
-    var lang = topic;
-    console.log(`I want to learn ${lang}`)
+if (true) {
+    var b = 10;
 }
+console.log(b);
 
-learn("python")
 
-console.log(`I know  ${lang}`)
+// var is a function scoped so it works fine inside a function . As I havend't declred it within any function it is first declaring it on the global level
+// then it is assigning it self inside the if statement  . The scenario is happending here is
 
+// var b;
+
+// if (true) {
+//     var b = 10;
+// }
+// console.log(b); 
