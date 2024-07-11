980f5c38579e0a95c3d7418bfc46782d1dc90cda
shariar <shariarmahmud225@gmail.com>
2024-07-11 18:55:01 +0600

    global schope variable with var

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Shariar Mahamud Duke")
+var lang = "javaScript"
+// global schope
+
+function learn(topic) {
+    // er modhe ja kisu lekah hbe seta holo function schope 
+
+    lang = topic;
+    console.log(`I want to learn ${lang}`)
+}
+
+learn("python")
+
+console.log(`I know  ${lang}`)
+
+// ekhane duta console log a eki variable er variable  k mutate kortse 
