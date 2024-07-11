c04c3ce996769a0a005a3b7ef7bccbe271337baf
shariar <shariarmahmud225@gmail.com>
2024-07-11 19:43:10 +0600

    premitive and reference value

--- index.js
+++ index.js
@@ -1,29 +1,16 @@
-// If I want to return a object from an function what should I do ?
+// Premitive variable string, number , boolean 
+// Premetive gular collection dia array , object crete hoy 
 
+let a = ["Shariar", "Duke"]
+let b = ["HTML", "CSS"];
 
-const hello = () =>
-(
-    {
-        a: 10,
-        b: 20
-    }
-)
+b = a;
+b.push("js")
+console.log(a)
+console.log(b)
 
-console.log(hello())
+// result
+// [ 'Shariar', 'Duke', 'js' ]
+// [ 'Shariar', 'Duke', 'js' ]
 
-// jode amk response hisebe ekta object send kore dite hoy tahole amk => er por {a:5, b:5} dile hbe na tahle eta k object er bracket vabbe na vabbe holo new block of code tai object
-// patahte gele age { } er age () dite hbe jate ei {} take object er bracket vabe
-
-
-// Antoher way 
-
-const hi = () => {
-    return {
-        a: 10,
-        b: 20
-    }
-}
-
-console.log(hi())
-
-// jdoe arrow er por {} use krei thake tahole pore r ekta return use kore nite hbe 
+// dutar resutl same karon b and a ekhn same memory space share kortse tai resutl o same 
