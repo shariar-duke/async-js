2ff5f678abdd6361940ab185a27c49e02117c718
shariar <shariarmahmud225@gmail.com>
2024-07-14 09:04:35 +0600

    example of a syncrouns function that will block every other code and freez the UI for a few moments, that's why async is introduced

--- index.js
+++ index.js
@@ -1,56 +1,9 @@
-// Refactoring the call back example with js promise 
-
-
-function getStudent(id) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log("Fetching data from Database ...")
-            resolve({ id: id, name: "Rahim" })
-
-        }, 2000)
-    })
-
+function blockingFunction() {
+    // Simulating a long operation
+    const start = Date.now();
+    while (Date.now() - start < 5000) { }  // Blocks for 5 seconds
+    console.log("Finished blocking function");
 }
 
-
-function getCourses(student) {
-
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log("Student courses from Database ...")
-            resolve({ id: student.id, name: student.name, courses: ["js", "python"] })
-        }, 1500)
-    })
-
-}
-
-function getQuizMarks(courses) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log("marks")
-            resolve({
-                [courses[0]]: 20,
-                [courses[1]]: 14
-            })
-
-        }, 1200)
-    })
-
-}
-
-
-// ekhne call korar somoy fist ekta promise k call korbo . or then theke or positive result ta ber kore anbo . Then or modhe abar porer function take call kro jno . ei promise
-// er ses a thn korle r ekta promise pawa jay. karon j function gula return korbo ogula  asole a promise a return kroe 
-
-getStudent(2).then((student) => {
-    console.log("The student is", student)
-    return getCourses(student)
-}).then((sutdentDetails) => {
-    console.log("Student Details are", sutdentDetails)
-    return getQuizMarks(sutdentDetails.courses)
-}
-).then((marks) => {
-    console.log("The marks of the sutdent in each subject", marks)
-}).catch((err) => console.log("I found an error"))
-
-// I have done this And converted this from call back to promsie  This is more readable now and well structured 
+blockingFunction();  // UI is unresponsive during this time
+console.log("This won't execute until the blocking function completes");
